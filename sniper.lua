181274827486824ff0c4b49a13e15addf6bbd7b9
dreamingg <116192687+coolgamerkid332@users.noreply.github.com>
2025-07-14 19:49:56 -0500

    Create sniper.lua

---
+++ sniper.lua
@@ -0,0 +1,31 @@
+getgenv()._=
+"Join discord.gg/msgabv2t9Q | discord.gg/stando to get latest update ok bai | If you pay for this script you get scammed, this script is completely free ok"
+--[[
+
+To get started in "getgenv().Owner" put the username you want to control.
+
+!! \\ MAKE SURE THE OWNER AND OWNER ACCOUNT ARE IN SAME SERVER // !!
+
+--// Example \\
+Before --> getgenv().Owner = "OWNER_USERNAME_HERE"
+After  --> getgenv().Owner = "XK5NG"
+
+^^ --> This is just the example don't put the username herez]]
+
+getgenv().Owner = "SkyHyp3r2005"
+
+getgenv().Whitelist = { --Endless bot whitelist
+    1,
+    2,
+    3,
+    4
+}
+
+getgenv().Configuration = {
+    ['CrewID'] = 32570691,
+    ['AntiStomp'] = true,
+    ['LowGraphic'] = true,
+    ['HideScreen'] = false,
+    ['CustomPrefix'] = "!",
+    ['Fpscap'] = 60
+}loadstring(game:HttpGet("https://xk5ng.github.io/Sniper-Bot-V0.3"))()
