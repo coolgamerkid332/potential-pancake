f480f4dc4741ca5fd399e359c190a921778e85dc
dreamingg <116192687+coolgamerkid332@users.noreply.github.com>
2025-07-31 23:10:38 -0500

    Update sniper.lua

--- sniper.lua
+++ sniper.lua
@@ -2,15 +2,15 @@ getgenv()._=
 "Join discord.gg/msgabv2t9Q | discord.gg/stando to get latest update ok bai | If you pay for this script you get scammed, this script is completely free ok"
 --[[
 
-To get started in "getgenv().Owner" put the username you want to control.
+To get started in "getgenv().Owner" put the username you want to control the alt.
 
-!! \\ MAKE SURE THE OWNER AND OWNER ACCOUNT ARE IN SAME SERVER // !!
+!! \\ MAKE SURE THE LocalPlayer AND LocalPlayer ACCOUNT ARE IN SAME SERVER // !!
 
 --// Example \\
 Before --> getgenv().Owner = "OWNER_USERNAME_HERE"
 After  --> getgenv().Owner = "XK5NG"
 
-^^ --> This is just the example don't put the username herez]]
+^^ --> This is just the example don't put the username here]]
 
 getgenv().Owner = "JulianByteLava"
 
@@ -22,10 +22,21 @@ getgenv().Whitelist = { --Endless bot whitelist
 }
 
 getgenv().Configuration = {
-    ['CrewID'] = 32570691,
-    ['AntiStomp'] = true,
-    ['LowGraphic'] = true,
-    ['HideScreen'] = false,
-    ['CustomPrefix'] = "!",
-    ['Fpscap'] = 60
-}loadstring(game:HttpGet("https://xk5ng.github.io/Sniper-Bot-V0.3"))()
+    ['Guns'] = {"Rifle", "Flintlock"}, --Default gun, recomended
+
+    ['Prediction'] = 1, --Prediction, self explainatory
+    ['Multiplier'] = 3, --Multiplier, multiplies the prediction value; dont change if you dont know what this does
+
+    ['StompHeight'] = 5, --Stomp Y offset, self explainatory; affects stomp Z X offset
+    ['RandomStompOffset'] = 0, --Stomp Z X offset, 0 = nil or disable; recomended for alts; '5' value is max for dh
+
+    ['SpareAmmo'] = 8, --How much ammo til start buying
+    ['ForceAmmo'] = 4, --How much ammo to force buying
+
+    ['CrewID'] = 32570691, --Crew id, preferable to be in both same crew alt/main
+    ['AntiStomp'] = true, --Self explainatory
+    ['LowGraphic'] = true, --Self explainatory
+    ['HideScreen'] = false, --Boost fps and lowered cpu consumption, recomended
+    ['CustomPrefix'] = "!", --Bot prefix
+    ['Fpscap'] = 60 --Capped fps
+}loadstring(game:HttpGet("https://xk5ng.github.io/Sniper-Bot-Rewrite"))()
