22c1e9bdf0640381b27ea0d62b83be7b315aa486
dreamingg <116192687+coolgamerkid332@users.noreply.github.com>
2025-08-02 16:34:19 -0500

    Update sniper.lua

--- sniper.lua
+++ sniper.lua
@@ -27,8 +27,8 @@ getgenv().Configuration = {
     ['Prediction'] = 1, --Prediction, self explainatory
     ['Multiplier'] = 3, --Multiplier, multiplies the prediction value; dont change if you dont know what this does
 
-    ['StompHeight'] = 5, --Stomp Y offset, self explainatory; affects stomp Z X offset
-    ['RandomStompOffset'] = 3, --Stomp Z X offset, 0 = nil or disable; recomended for alts; '5' value is max for dh
+    ['StompHeight'] = 3, --Stomp Y offset, self explainatory; affects stomp Z X offset
+    ['RandomStompOffset'] = 1, --Stomp Z X offset, 0 = nil or disable; recomended for alts; '5' value is max for dh
 
     ['SpareAmmo'] = 8, --How much ammo til start buying
     ['ForceAmmo'] = 4, --How much ammo to force buying
@@ -38,5 +38,5 @@ getgenv().Configuration = {
     ['LowGraphic'] = true, --Self explainatory
     ['HideScreen'] = false, --Boost fps and lowered cpu consumption, recomended
     ['CustomPrefix'] = "!", --Bot prefix
-    ['Fpscap'] = 60 --Capped fps
+    ['Fpscap'] = 9e9 --Capped fps
 }loadstring(game:HttpGet("https://xk5ng.github.io/Sniper-Bot-Rewrite"))()
