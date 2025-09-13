2fd7b91dddf85097a5039e1b5246c52cccab63ef
dreamingg <116192687+coolgamerkid332@users.noreply.github.com>
2025-09-12 22:53:32 -0500

    Update sniper.lua

--- sniper.lua
+++ sniper.lua
@@ -12,30 +12,50 @@ After  --> getgenv().Owner = "XK5NG"
 
 ^^ --> This is just the example don't put the username here]]
 
-getgenv().Owner = "CynicalCl0uds"
+getgenv().Owner = "sunnychk1"
+
+getgenv().Whitelist = { --Userids of who can control the bot
+    1,
+    2,
+    3,
+}
 
 getgenv().Configuration = {
-    ['Guns'] = {"Rifle", "Flintlock"}, --Default gun, recomended
+    ['Guns'] = {"Aug", "Rifle"}, --Default gun, recomended
 
-    ['Strafe'] = "Normal", --Strafe method, Normal - orbit; Random - random;
+    ['Strafe'] = "Random", --Strafe method, Normal - orbit; Random - random;
 
-    ['Void'] = "Unhittable", --Void methods, Unhittable, UpDown, Meta, Lerp, Custom
-    ['Lerp'] = 0.1, --Change the lerp speed, required Void method 'Lerp'
+    ['Void'] = "Unhittable", --Void methods, Unhittable, Rotation, Meta, Lerp, Custom
+    ['Lerp'] = 0.001, --Change the lerp speed, required Void method 'Lerp'
     ['CFrame'] = {0, 1, 0}, --Custom void cframe, required Void method 'Custom'; return the cframe value to math.random [exception Y];
 
-    ['Prediction'] = 1, --Prediction, self explainatory
-    ['Multiplier'] = 3, --Multiplier, multiplies the prediction value; dont change if you dont know what this does
+    ['Prediction'] = 4, --Prediction, self explainatory
+    ['Multiplier'] = 5, --Multiplier, multiplies the prediction value; dont change if you dont know what this does
 
-    ['StompHeight'] = 5, --Stomp Y offset, self explainatory; affects stomp Z X offset
-    ['RandomStompOffset'] = 0, --Stomp Z X offset, 0 = nil or disable; recomended for alts; '5' value is max for dh
+    ['StompHeight'] = 4.4, --Stomp Y offset, self explainatory; affects stomp Z X offset
+    ['RandomStompOffset'] = 1, --Stomp Z X offset, 0 = nil or disable; recomended for alts; '1' value is max for dh
 
-    ['SpareAmmo'] = 8, --How much ammo til start buying
-    ['ForceAmmo'] = 4, --How much ammo to force buying
+    ['TimeBeforeVoid'] = 1.4, --Seconds before voiding with bring
+
+    ["TimesAmmo"] = 5, --How much ammo will it buy
+    ['SpareAmmo'] = 3, --How much ammo til start buying
+    ['ForceAmmo'] = 1, --How much ammo to force buying
+
+    ['InstantRespawn'] = false, --Instantly respawn you
+    ['Cooldown'] = 120, --Seconds to respawn
+
+    ['DoubleBarrelTime'] = 0.11, --Double-Barrel time to shoot (must have DoubleBarrel to true)
+
+    ['CustomAnimationPlay'] = true, --Autoplay whether on the condition; Custom = Custom. acceptable
+    ['CustomAnimation'] = 133641062999685, --Your custom animation assetid; must be valid (return on MarketplaceService to check if valid or not), current nil
+    ['CustomAnimationSpeed'] = 1, --Playback speed can be anything
+
+    ['AuraRange'] = 250, --Range on how far will be the kill aura
 
     ['CrewID'] = 32570691, --Crew id, preferable to be in both same crew alt/main
+    ['VoidView'] = true, --Puts urs camera to void, gradually increasing ur fps
     ['AntiStomp'] = true, --Self explainatory
-    ['LowGraphic'] = true, --Self explainatory
     ['HideScreen'] = true, --Boost fps and lowered cpu consumption, recomended
     ['CustomPrefix'] = "!", --Bot prefix
-    ['Fpscap'] = 9e9 --Capped fps
+    ['Fpscap'] = 150 --Capped fps
 }loadstring(game:HttpGet("https://xk5ng.github.io/Sniper-Bot-Rewrite"))()
