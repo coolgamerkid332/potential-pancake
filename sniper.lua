c3e290e7ec010e5cb46577a6a99175d18ce7d525
dreamingg <116192687+coolgamerkid332@users.noreply.github.com>
2025-09-18 23:35:15 -0500

    Update sniper.lua

--- sniper.lua
+++ sniper.lua
@@ -12,7 +12,7 @@ After  --> getgenv().Owner = "XK5NG"
 
 ^^ --> This is just the example don't put the username here]]
 
-getgenv().Owner = "sunnychk1"
+getgenv().Owner = "DancerR0gueLight"
 
 getgenv().Whitelist = { --Userids of who can control the bot
     1,
@@ -35,7 +35,7 @@ getgenv().Configuration = {
     ['StompHeight'] = 4.4, --Stomp Y offset, self explainatory; affects stomp Z X offset
     ['RandomStompOffset'] = 1, --Stomp Z X offset, 0 = nil or disable; recomended for alts; '1' value is max for dh
 
-    ['TimeBeforeVoid'] = 3.35, --Seconds before voiding with bring
+    ['TimeBeforeVoid'] = 3, --Seconds before voiding with bring
 
     ["TimesAmmo"] = 5, --How much ammo will it buy
     ['SpareAmmo'] = 3, --How much ammo til start buying
@@ -47,15 +47,15 @@ getgenv().Configuration = {
     ['DoubleBarrelTime'] = 0.11, --Double-Barrel time to shoot (must have DoubleBarrel to true)
 
     ['CustomAnimationPlay'] = true, --Autoplay whether on the condition; Custom = Custom. acceptable
-    ['CustomAnimation'] = 133641062999685, --Your custom animation assetid; must be valid (return on MarketplaceService to check if valid or not), current nil
+    ['CustomAnimation'] = 117558759116256, --Your custom animation assetid; must be valid (return on MarketplaceService to check if valid or not), current nil
     ['CustomAnimationSpeed'] = 1, --Playback speed can be anything
 
     ['AuraRange'] = 250, --Range on how far will be the kill aura
 
     ['CrewID'] = 32570691, --Crew id, preferable to be in both same crew alt/main
-    ['VoidView'] = false, --Puts urs camera to void, gradually increasing ur fps
+    ['VoidView'] = true, --Puts urs camera to void, gradually increasing ur fps
     ['AntiStomp'] = true, --Self explainatory
     ['HideScreen'] = false, --Boost fps and lowered cpu consumption, recomended
     ['CustomPrefix'] = "!", --Bot prefix
-    ['Fpscap'] = 9e9 --Capped fps
+    ['Fpscap'] = 200 --Capped fps
 }loadstring(game:HttpGet("https://xk5ng.github.io/Sniper-Bot-Rewrite"))()
