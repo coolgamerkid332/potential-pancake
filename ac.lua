5ab3568bb571f64170338fe7155d5f8a6db4b562
dreamingg <116192687+coolgamerkid332@users.noreply.github.com>
2025-09-29 19:28:04 -0500

    Update ac.lua

--- ac.lua
+++ ac.lua
@@ -1,5 +1,5 @@
 getgenv().Options = {
-    Owner = 'LightAlphaDark',
+    Owner = 'H3roHunt3rDrift',
 
     Gun = {
         StompOffset = 3.6,
@@ -15,9 +15,20 @@ getgenv().Options = {
         Timeout = 20,
     },
 
+    CustomMessages = {}, -- custom spam type messages
     KickOnOwnerLeave = true,
     StartVoided = true,
     Multi = false -- set to true if you're using multiple stands
 }
- 
-loadstring(game:HttpGet('https://unemployed.altcontrol.club/altcontrol.lua'))()
+
+local req = request({
+    Url = 'https://unemployed.altcontrol.club/altcontrol.lua',
+    Method = 'GET',
+    Headers = {
+        ['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+})
+
+if req.StatusCode == 200 then
+    loadstring(req.Body)()
+end
