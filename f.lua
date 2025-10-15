f5e6ef5da26e33724c31238c832055c3291cbe7a
dreamingg <116192687+coolgamerkid332@users.noreply.github.com>
2025-10-15 15:53:15 -0500

    Add initial configuration for script settings

---
+++ f.lua
@@ -0,0 +1,11 @@
+getgenv().Script = "Get forsh for free at discord.gg/wpHejFfW8k"
+
+getgenv().Owner = "H3roHunt3rDrift"
+getgenv().FPSCap = 60
+getgenv().HideScreen = false
+getgenv().Disable3DRendering = false
+getgenv().AbuseProtection = false
+getgenv().ExtraOptimizations = false
+getgenv().SelectedGuns = {"rifle", "aug", "flintlock"} -- rifle, aug, flintlock, lmg, db
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/farmoretype/Forsh/refs/heads/main/Loader"))()
